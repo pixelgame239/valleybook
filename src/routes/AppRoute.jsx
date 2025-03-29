6efe3803dff81f38b4f177c6d9e121756080f13b
Dang Hoang <hdjhdbsudg@gmail.com>
2025-03-29 22:57:39 +0700

    navigate contact

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import ContactPage from "../contact";
+
 const AppRoute = () => {
   return (
-      <Routes>
-        <Route path="/" element={<HomePage />} />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/Contact" element={<ContactPage />} />
 
-        {/* <Route path="/about" element={<About />} />
+      {/* <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} /> */}
-        {/* <Route path="*" element={<NotFound />} /> */}
-      </Routes>
+      {/* <Route path="*" element={<NotFound />} /> */}
+    </Routes>
   );
 };
 
