d8150ccf404d029b01a4705739c75ccd294ee274
Dang Hoang <hdjhdbsudg@gmail.com>
2025-05-06 23:36:36 +0700

    adjust close listen trial

--- src/components/SimpleAudioPlayer.jsx
+++ src/components/SimpleAudioPlayer.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import "./SimpleAudioPlayer.css";
 
 function AudioPlayer({ onClose }) {
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = 1; // Set volume to 50%
+    }
+  }, []);
+
   return (
     <div className="audio-player-container">
       <audio
+        ref={audioRef}
         controls
         autoPlay
         src="/assets/audio/sach-noi-giet-con-chim-nhai.mp3"
